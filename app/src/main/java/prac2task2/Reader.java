344ac1a39aa0d6f91ae3581f61e810b42b74b8f6
Sello Prince Lebudi <g22l1730@campus.ru.ac.za>
2024-05-22 05:22:26 +0200

    Switched to using a SAX XML parser

--- app/src/main/java/prac2task2/Reader.java
+++ app/src/main/java/prac2task2/Reader.java
@@ -1,53 +1,69 @@
 package prac2task2;
 import java.io.*;
-import org.w3c.dom.*;
+import org.xml.sax.*;
+import org.xml.sax.helpers.*;
 import javax.xml.parsers.*;
 import com.google.gson.JsonObject;
 
 public class Reader {
     public Reader() {}
     public void readAndPrint(String path, boolean printName, boolean printPostalZip, boolean printRegion, boolean printCountry, boolean printAddress) {
-        try{
-            File file = new File(path);     
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(file);
-            doc.getDocumentElement().normalize();
-            Element root = doc.getDocumentElement();
-            NodeList nodeList = root.getChildNodes();
-            Node node;
-
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                node = nodeList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    JsonObject jsonObject = new JsonObject();
-                    if (printName && "name".equals(node.getNodeName())) {
-                        String name = element.getTextContent();
-                        jsonObject.addProperty("Name", name);
-                    }
-                    if (printPostalZip && "postalZip".equals(node.getNodeName())) {
-                        String postalZip = element.getTextContent();
-                        jsonObject.addProperty("Postal/Zip", postalZip);
-                    }
-                    if (printRegion && "region".equals(node.getNodeName())) {
-                        String region = element.getTextContent();
-                        jsonObject.addProperty("Region", region);
-                    }
-                    if (printCountry && "country".equals(node.getNodeName())) {
-                        String country = element.getTextContent();
-                        jsonObject.addProperty("Country", country);
-                    }
-                    if (printAddress && "address".equals(node.getNodeName())) {
-                        String address = element.getTextContent();
-                        jsonObject.addProperty("Address", address);
-                    }
-                    System.out.println(jsonObject.toString());
-                }
-            }
-        }
-        catch(Exception e) {
+        try {
+            XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+            xmlReader.setContentHandler(new MyHandler(printName, printPostalZip, printRegion, printCountry, printAddress));
+            xmlReader.parse(new InputSource(new FileInputStream(path)));
+        } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private class MyHandler extends DefaultHandler {
+        private boolean printName;
+        private boolean printPostalZip;
+        private boolean printRegion;
+        private boolean printCountry;
+        private boolean printAddress;
+
+        private StringBuilder currentValue;
+        private JsonObject jsonObject;
+
+        public MyHandler(boolean printName, boolean printPostalZip, boolean printRegion, boolean printCountry, boolean printAddress) {
+            this.printName = printName;
+            this.printPostalZip = printPostalZip;
+            this.printRegion = printRegion;
+            this.printCountry = printCountry;
+            this.printAddress = printAddress;
+            this.currentValue = new StringBuilder();
+            this.jsonObject = new JsonObject();
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            currentValue.setLength(0);
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            currentValue.append(ch, start, length);
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if (printName && qName.equals("name")) {
+                jsonObject.addProperty("Name", currentValue.toString());
+            } else if (printPostalZip && qName.equals("postalZip")) {
+                jsonObject.addProperty("Postal/Zip", currentValue.toString());
+            } else if (printRegion && qName.equals("region")) {
+                jsonObject.addProperty("Region", currentValue.toString());
+            } else if (printCountry && qName.equals("country")) {
+                jsonObject.addProperty("Country", currentValue.toString());
+            } else if (printAddress && qName.equals("address")) {
+                jsonObject.addProperty("Address", currentValue.toString());
+            }
+            if (!jsonObject.entrySet().isEmpty()) {
+                System.out.println(jsonObject.toString());
+                jsonObject = new JsonObject();
+            }
         }
     }
 }
