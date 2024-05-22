995bd56b24f9416d6535d7bb1c9dc922e0be5a2b
Sello Prince Lebudi <g22l1730@campus.ru.ac.za>
2024-05-22 03:16:33 +0200

    Changed the  reader method to output fields specified by the user in the App main  method

--- app/src/main/java/prac2task2/App.java
+++ app/src/main/java/prac2task2/App.java
@@ -7,7 +7,14 @@ import prac2task2.Reader;
 public class App {
     public static void main(String[] args) {
         Reader reader = new Reader();
+
+        boolean printName = true;
+        boolean printPostalZip = false; // Change to true if you want to print
+        boolean printRegion = true;
+        boolean printCountry = false; // Change to true if you want to print
+        boolean printAddress = true;
+        
         String path = "C:/Users/Sello Prince Lebudi/Desktop/Prac2Task2/Prac2.xml";
-        reader.readAndPrint(path);
+        reader.readAndPrint(path, printName, printPostalZip, printRegion, printCountry, printAddress);
     }
 }
--- app/src/main/java/prac2task2/Reader.java
+++ app/src/main/java/prac2task2/Reader.java
@@ -5,7 +5,7 @@ import javax.xml.parsers.*;
 
 public class Reader {
     public Reader() {}
-    public void readAndPrint(String path) {
+    public void readAndPrint(String path, boolean printName, boolean printPostalZip, boolean printRegion, boolean printCountry, boolean printAddress) {
         try{
             File file = new File(path);     
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -14,11 +14,32 @@ public class Reader {
             doc.getDocumentElement().normalize();
             Element root = doc.getDocumentElement();
             NodeList nodeList = root.getChildNodes();
+            Node node;
 
             for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
+                node = nodeList.item(i);
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    System.out.println(node.getNodeName() + ": " + node.getTextContent());
+                    Element element = (Element) node;
+                    if (printName && "name".equals(node.getNodeName())) {
+                        String name = element.getTextContent();
+                        System.out.println("Name: " + name);
+                    }
+                    if (printPostalZip && "postalZip".equals(node.getNodeName())) {
+                        String postalZip = element.getTextContent();
+                        System.out.println("Postal/Zip: " + postalZip);
+                    }
+                    if (printRegion && "region".equals(node.getNodeName())) {
+                        String region = element.getTextContent();
+                        System.out.println("Region: " + region);
+                    }
+                    if (printCountry && "country".equals(node.getNodeName())) {
+                        String country = element.getTextContent();
+                        System.out.println("Country: " + country);
+                    }
+                    if (printAddress && "address".equals(node.getNodeName())) {
+                        String address = element.getTextContent();
+                        System.out.println("Address: " + address);
+                    }
                 }
             }
         }
